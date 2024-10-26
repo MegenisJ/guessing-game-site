07184b805426e46d027b573130721ca09d1aab8d
JMegenis <james@megenis.com>
2024-10-26 01:12:50 +0100

    update formatting

--- gameState.go
+++ gameState.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
-	_ "github.com/mattn/go-sqlite3"
+    "database/sql"
+    "fmt"
+    "log"
+    "os"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func setupState(){
     os.Remove("./foo.db")
     db, err := sql.Open("sqlite3", "./foo.db")
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	defer db.Close()
+    defer db.Close()
 
     sqlStmt := `
-        create table foo (id integer not null primary key, state integer);
-        delete from foo;
-	`
-	_, err = db.Exec(sqlStmt)
+    create table foo (id integer not null primary key, state integer);
+    delete from foo;
+    `
+    _, err = db.Exec(sqlStmt)
 
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
+    if err != nil {
+        log.Printf("%q: %s\n", err, sqlStmt)
+        return
+    }
     tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	stmt, err := tx.Prepare("insert into foo(id, state) values(?, ?)")
+    stmt, err := tx.Prepare("insert into foo(id, state) values(?, ?)")
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	defer stmt.Close()
+    defer stmt.Close()
     _,err = stmt.Exec(1,10)
     if err != nil {
-            log.Fatal(err)
+        log.Fatal(err)
     }
 
     err = tx.Commit()
